7ac963a2647bb546e98d9c15dff9c570ade014e8
Documenter.jl <documenter@juliadocs.github.io>
2022-01-26 01:08:51 +0000

    build based on 456f9ff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ssht.jl:-Fast-and-exact-spin-spherical-harmonic-transforms","page":"ssht.jl: Fast and exact spin spherical harmonic transforms","title":"ssht.jl: Fast and exact spin spherical harmonic transforms","text":"","category":"section"},{"location":"","page":"ssht.jl: Fast and exact spin spherical harmonic transforms","title":"ssht.jl: Fast and exact spin spherical harmonic transforms","text":"This package is a Julia wrapper for the ssht library. It calculates spin spherical harmonic transforms.","category":"page"}]
+}
